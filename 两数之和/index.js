53014e34847babd51e52bed496c54d9beddbd42b
lidongyang <752860629@qq.com>
2022-04-01 21:57:05 +0800

    add 两数之和解答

---
+++ 两数之和/index.js
@@ -0,0 +1,48 @@
+/** 计算两数之和
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number []}
+ */
+
+// 第一种解法：
+
+function twoSum(nums, target) {
+  // 思路 两次循环遍历数组逐一相加对比。
+  for (let index = 0; index < nums.length; index++) {
+    for (let temIndex = index + 1; temIndex < nums.length - 1; temIndex++) {
+      if (target === nums[index] + nums[temIndex]) {
+        console.log([index, temIndex]);
+        return [index, temIndex];
+      }
+    }
+  }
+  return [];
+}
+
+// twoSum([2, 7, 11, 15], 9);
+
+// 第二种解法思路：
+// 根据target 减去当前的值，来查看余数是否在Map里。如果在 把对应value和当前的index取出来即可。
+// 如果不在map里，把当前的值和下标放进Map里。
+// 省去了一次循环。
+
+/**
+ * 计算两数之和优化
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number []}
+ */
+function twoSumRefactor(nums, target) {
+  let map = new Map();
+  for (let index = 0; index < nums.length; index++) {
+    const another = target - nums[index];
+    if (map.has(another)) {
+      return [map.get(another), index];
+    }
+    map.set(nums[index], index);
+  }
+}
+
+twoSumRefactor([2, 7, 11, 15], 9);
